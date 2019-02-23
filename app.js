8615bded328f113ec5d83c7f674bcbccae489894
root <root@localhost.localdomain>
2019-02-23 12:01:05 +0200

    version 1

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.get('/',function(req,res){
 
 })
 
+app.get('/gallery',function(req,res){
+	res.render('gallery')
+
+})
+app.get('/quiz',function(req,res){
+	res.render('quiz')
+
+})
+app.get('*',function(req,res){
+	res.send('<center><h1>PAGE NOT FOUND</h1></center>')
+
+})
+
 app.listen(5000,'0.0.0.0',function(){console.log(
 	'stem server started !!')})
 
